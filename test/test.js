d4aaadd7a597df72476432ab0c23223927ab0900
Mathieu Lecarme <mlecarme@bearstech.com>
2013-02-14 23:45:35 +0100

    Clean before test.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,16 @@
-var should = require('should'),
+var fs = require('fs'),
+    should = require('should'),
     jsonStore = require('../lib/bokkusu/jsonstore').store;
 
 describe('bokkusu', function() {
+    var PATH = '/tmp/toto.json';
+
+    beforeEach(function() {
+        fs.unlinkSync(PATH);
+    });
+
     it('should store something', function(done) {
-        jsonStore('/tmp/toto.js', function() {
+        jsonStore(PATH, function() {
             var bokkusu = this;
             bokkusu.bulk({'1': 'robert', '2': 'paul'}, function(error) {
                 bokkusu.get('2', function(err, value) {
