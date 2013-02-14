43e8c43893da90a82abfe2959eb8f100cefc140b
Mathieu Lecarme <mlecarme@bearstech.com>
2013-02-14 23:32:59 +0100

    Mocha and key leaking.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,15 @@
-var jsonStore = require('../lib/bokkusu/jsonstore').store;
+var should = require('should'),
+    jsonStore = require('../lib/bokkusu/jsonstore').store;
 
 describe('bokkusu', function() {
-    it('should store something', function() {
+    it('should store something', function(done) {
         jsonStore('/tmp/toto.js', function() {
             var bokkusu = this;
             bokkusu.bulk({'1': 'robert', '2': 'paul'}, function(error) {
-                console.log(bokkusu.store.store);
-                asyncSpecDone();
+                bokkusu.get('2', function(err, value) {
+                    value.should.eql('paul');
+                    done();
+                });
             });
         });
     });
